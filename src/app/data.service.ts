7ef145a7660e9765eaeb840dcaa1e074e095a75e
Rojesh Dangol <rozs@Rojeshs-MacBook-Pro.local>
2021-02-03 18:39:06 -0500

    Http get put working

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,26 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { iData } from './data';
+import { Observable, throwError} from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable ({
-    providedIn : "root";
+    providedIn : "root"
 })
 
 
 export class DataService {
-    
-    data= [{
-        name: {fname: "Rojesh", lName: "Dangol"};
-        address: {street: "816 Perry Hwy",
-                    apt: 24,
-                    city: 'Pittsburgh',
-                    state: 'PA',
-                    zip: 15229},
-        contact: { email: 'abc@gmail.com',
-                    phone: 12312321}
+
+
+
+    public url: string = "http://localhost:3000/dataArray";
+
+    constructor(private http: HttpClient){}
+
+    getData(): Observable<iData[]>{
+        return this.http.get<iData[]>(this.url);
     }
-    ]
 
+    updateData(id: number, newInfo: any){
+        
+        this.http.put( `http://localhost:3000/dataArray/${id}`, newInfo).subscribe(data => {console.log('updated')});
+    }
 
-    get(){
+    /*get(){
        return this.data;
     }
 
@@ -29,10 +36,10 @@ export class DataService {
     }
 
     delete(newData: any){
-        const index: this.data.indexOf(newData);
-        if(index >= 0){
+        const index= this.data.indexOf(newData);
+        if( index >= 0 ){
             this.data.splice(index, 1);
         }
-    }
+    }*/
 
 }
---
+++ src/app/data.ts
@@ -0,0 +1,11 @@
+export interface iData{
+    id: number,
+    first: string,
+    last: string,
+    street: string,
+    apt: number,
+    city: string,
+    state: string,
+    zip: number
+
+}
